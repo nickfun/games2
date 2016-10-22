cfccfb3ada0f6a38a004d49de141c11e3e49d8a3
Nicholas Funnell <nfunnell@twilio.com>
2016-10-22 11:25:02 -0700

    playing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// main.js
-var React = require('react');
-var ReactDOM = require('react-dom');
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = (props) => {
     return (<div><h1>ES6 & React Games App v2</h1><p>&copy; Nick Funnell</p><p>Insert Games Here</p></div>);
